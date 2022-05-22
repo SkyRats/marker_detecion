2675311700770b8af6e6dacf70b45207fe626bd6
sophi011 <sophiaceline@usp.br>
2022-05-22 20:49:20 -0300

    aruco generator and detector implemented

--- scripts/markerdetection.py
+++ scripts/markerdetection.py
@@ -2,6 +2,15 @@ import numpy as np
 from pyzbar.pyzbar import decode
 import cv2
 
+import argparse
+import sys
+
+ARUCO_DICT = {
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+}
 
 class MarkerDetection():
     
@@ -12,6 +21,9 @@ class MarkerDetection():
         self.qr_debug = True
         self.frame = None
         self.det_number = 0
+        self.gen_aruco = None
+        self.aruco_dic = cv2.aruco.DICT_5X5_250
+        self.aruco_id = None
 
     def qrdetection(self, vid):
         ret, self.frame = vid.read()
@@ -48,10 +60,35 @@ class MarkerDetection():
             cv2.imshow('frame', self.frame)
 
 
+    def aruco_generator(self, id):
+        
+        self.aruco_id = id
+        # Load the predefined dictionary
+        dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_250)
+
+        # Generate the marker
+        markerImage = np.zeros((200, 200), dtype=np.uint8)
+        markerImage = cv2.aruco.drawMarker(dictionary, self.aruco_id, 800, markerImage, 2)
+        cv2.imwrite("marker2.png", markerImage)
+        self.gen_aruco = "marker2.png"
+
+
     def aruco_detection(self):
-        pass
-    
+
+        self.frame = cv2.imread("/home/soph/skyrats_ws/src/marker_detecion/scripts/aruco2.png")
+        #Load the dictionary that was used to generate the markers.
+        dictionary = cv2.aruco.Dictionary_get(self.aruco_dic)
+        # Initialize the detector parameters using default values
+        parameters =  cv2.aruco.DetectorParameters_create()
+        # Detect the markers in the image
+        markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(self.frame, dictionary, parameters=parameters)
+        print("Marker corners\n", markerCorners)
+        print("MarkerIds\n", markerIds)
+
 
 if __name__ == "__main__":
     detectiontest = MarkerDetection()
-    detectiontest.qrtest()
+    #detectiontest.qrtest()
+    detectiontest.aruco_generator(2)
+    
+    detectiontest.aruco_detection()
