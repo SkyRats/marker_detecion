234b6403bce31cef5315abbf1ce265245bf410fa
alessandro <alecmenegon@gmail.com>
2022-07-26 19:24:42 -0300

    aruco, cross and centralization

--- scripts/aruco_script.py
+++ scripts/aruco_script.py
@@ -1,44 +1,30 @@
 #!/usr/bin/env python3
-
+import time
 import rclpy
 from markerdetection import MarkerDetection
 import sys
-sys.path.insert(0,'/home/software/skyrats_ws2/src/mavbase2')
-#print(sys.path)
+sys.path.insert(0,'/home/alessandro/skyrats_ws2/src/mavbase2')
 from MAV2 import MAV2
-import numpy as np
-
-TOL = 0.4
-
-def run():
-
-    rclpy.init()
-    rclpy.create_node("ArucoDetection")
-    rclpy.get_global_executor()
-    mav = MAV2()
-    detection = MarkerDetection()
-    detection.camera_topic = "/camera/image_raw"
-    cam_id = 4
-    actual_x = mav.drone_pose.pose.position.x
-    actual_y = mav.drone_pose.pose.position.y
-    actual_z = mav.drone_pose.pose.position.z
-
-    # aruco3 = (0, 0, 0) | aruco5 = (5, 5, 0) | aruco9 = (7, 0, 0) | aruco10 = (0, 7, 0) | aruco11 = (-3, 7, 0)
-    pose_vec = [[0, 0, 0], [5, 5, 0], [7, 0, 0], [0, 7, 0], [-3, 7, 0]]
-    mav.takeoff(3)
-    rclpy.spin_once(mav)
-    for tag_pose in range(len(pose_vec)):
-        goal_x = pose_vec[tag_pose][0]
-        goal_y = pose_vec[tag_pose][1]
-
-        dist = np.sqrt((goal_x - actual_x)**2 + (goal_y - actual_y)**2)
-        mav.get_logger().info("Moving to tag position...")
-        mav.go_to_local(goal_x, goal_y, 3)
-        mav.get_logger().info("On position")
-        detection.aruco_detection(mav.cam)
-        rclpy.spin_once(mav)
-
-if __name__ == "__main__":
-    run()
 
 
+# Utilize a função centralize_on_aruco fornecendo o mav e as coordenadas do aruco
+# Ao fornecer também um ID, o drone centraliza apenas no aruco desejado
+# Isso pode ser útil no caso de uma varredura
+
+rclpy.init()
+rclpy.create_node("ArucoDetection")
+rclpy.get_global_executor()
+mav = MAV2()
+detection = MarkerDetection()
+detection.camera_topic = "/camera/image_raw"
+
+pose_vec = [(1, 1, 0), (5.2, 4.8, 0), (7.2, 0.3, 0), (1, 6, 0), (-2.8, 7.2, 0)]
+rclpy.spin_once(mav)
+
+for tag in pose_vec:
+    detection.centralize_on_aruco(mav, tag)
+    time.sleep(5)
+
+
+
+ 
--- scripts/markerdetection.py
+++ scripts/markerdetection.py
@@ -1,11 +1,12 @@
+from imutils.video import VideoStream	
+import imutils				
 import numpy as np
 from pyzbar.pyzbar import decode
 import cv2
 import argparse
 import sys
-from imutils.video import VideoStream	# Webcam
-import imutils				# Resize images
-import time				# Delay
+import time			
+import rclpy
 
 ARUCO_DICT = {
 	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
@@ -24,13 +25,18 @@ class MarkerDetection():
         self.frame = None
         self.det_number = 0
         self.gen_aruco = None
-        self.aruco_dic = cv2.aruco.DICT_5X5_250
+        self.aruco_dic = cv2.aruco.DICT_5X5_1000
+        self.arucoDict = cv2.aruco.Dictionary_get(self.aruco_dic)
+        self.arucoParams = cv2.aruco.DetectorParameters_create()
         self.aruco_id = None
         self.aruco_debug = False
         self.qr_x = 0
         self.qr_y = 0
         self.qr_w = 0
         self.qr_h = 0
+        self.TOL = 0.0140
+        self.PID = 1/2000
+        self.TARGET = (500, 400)
 
     def qrdetection(self, vid):
         ret, self.frame = vid.read()
@@ -84,95 +90,150 @@ class MarkerDetection():
         markerImage = cv2.aruco.drawMarker(dictionary, self.aruco_id, aruco_size, markerImage, 1)
         self.gen_aruco = cv2.copyMakeBorder(markerImage, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT, value=[255, 255, 255])
         cv2.imwrite("markerID="+str(id)+".png", self.gen_aruco)
+    
 
-    def aruco_detection(self, cam):
+    def detect_arucos(self, frame, id=None):
 
-        #self.frame = self.gen_aruco
-        self.detection = True
-        #vs = VideoStream(cam_id).start()
-        vs = cam
+        frame = imutils.resize(frame, width=1000)
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, self.arucoDict, parameters=self.arucoParams)
+        lista_arucos = []
 
-        #Load the dictionary that was used to generate the markers.
-        dictionary = cv2.aruco.Dictionary_get(self.aruco_dic)
-
-        arucoParams = cv2.aruco.DetectorParameters_create()
-
-        first_detection = True
-
-        while self.detection and self.det_number <10: #and self.det_number <= 150:
-
-            # grab the frame from the threaded video stream and resize it to have a maximum width of 1000 pixels
-            frame = cam
-            frame = imutils.resize(frame, width=1000)
-
-            # detect ArUco markers in the input frame
-            (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, dictionary, parameters=arucoParams)
+        if len(corners) > 0:
             
-            # verify *at least* one ArUco marker was detected
-            if len(corners) > 0:
-                
-                self.det_number += 1
-                # flatten the ArUco IDs list
-                ids = ids.flatten()
+            ids = ids.flatten()
 
-                # loop over the detected ArUCo corners
-                for (markerCorner, markerID) in zip(corners, ids):
+            for (markerCorner, markerID) in zip(corners, ids):
 
-                    # extract the marker corners
-                    corners = markerCorner.reshape((4, 2))
-                    (topLeft, topRight, bottomRight, bottomLeft) = corners
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
 
-                    # convert each of the (x, y)-coordinate pairs to integers
-                    topRight = (int(topRight[0]), int(topRight[1]))
-                    bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-                    bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-                    topLeft = (int(topLeft[0]), int(topLeft[1]))
+                tR = (int(topRight[0]), int(topRight[1]))
+                bR = (int(bottomRight[0]), int(bottomRight[1]))
+                bL = (int(bottomLeft[0]), int(bottomLeft[1]))
+                tL = (int(topLeft[0]), int(topLeft[1]))
 
-                    # draw the bounding box of the ArUCo detection
-                    cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
-                    cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
-                    cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
-                    cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
-            
-                    # compute and draw the center (x, y)-coordinates of the ArUco marker
-                    cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-                    cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-                    cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+                area = ( (tR[0]*bR[1] - bR[0]*tR[1]) + (bR[0]*bL[1] - bL[0]*bR[1]) + (bL[0]*tL[1] - tL[0]*bL[1]) + (tL[0]*tR[0] - tR[0]*tL[1]) ) / 2
 
-                    # draw the ArUco marker ID on the frame
-                    cv2.putText(frame, str(markerID),
-                        (topLeft[0], topLeft[1] - 15),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (0, 255, 0), 2)
+                cX = int((tR[0] + bR[0]) / 2.0)
+                cY = int((tL[1] + bR[1]) / 2.0)
 
-                self.aruco_id = ids
-                number_of_arucos = len(self.aruco_id)
-                last_aruco_detected = self.aruco_id[number_of_arucos-1]
-                if first_detection:
-                    print("ArUco ID: ", self.aruco_id)
-                    first_detection = False
-                    old_arucos = self.aruco_id
+                if id == None:
+                    lista_arucos.append([markerID, (cX, cY), area])
                 else:
-                    for old_aruco in old_arucos:
-                        for new_aruco in self.aruco_id:
-                            if old_aruco != new_aruco:
-                                first_detection = True
+                    if markerID == id:
+                        lista_arucos.append([markerID, (cX, cY), area])
 
-            # show the output frame
-            cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1) & 0xFF
+        return lista_arucos
 
-            # if the `q` key was pressed, break from the loop
-            if key == ord("q"):
-                break
 
-        # cleanup
-        cv2.destroyAllWindows()
+    def centralize_on_aruco(self, drone, tag, aruco_id=None):
+
+        goal_x = tag[0]
+        goal_y = tag[1]
+        goal_z = tag[2] + 2.0
+
+        drone.get_logger().info("Moving to aruco region...")
+        drone.go_to_local(goal_x, goal_y, goal_z)
+        drone.get_logger().info("On position, waiting for stabilization...")
+        
+        time.sleep(5)
+
+        drone.get_logger().info("Drone stable, starting centralization..")
+        is_centralize = False
+
+        while not is_centralize:
+            
+            aruco_detected = False
+            timer = 0
+            no_detection = 0
+            while not aruco_detected:
+                frame = drone.cam
+                list_of_arucos = self.detect_arucos(frame, aruco_id)
+
+                if len(list_of_arucos) > 0:
+                    aruco_detected = True
+                    timer = 0
+
+                if timer > 500:
+                    drone.set_vel(0, 0, 0.1)
+                    print("No visible Arucos, going up...")
+                    timer = 0
+                    no_detection += 1
+
+                if no_detection > 100:
+                    print("Aruco not found! Going back to starting position...")
+                    drone.go_to_local(0, 0, goal_z)
+                    return
+
+                timer += 1
+                rclpy.spin_once(drone)
+
+            aruco = list_of_arucos[0]
+            markerID = aruco[0]
+
+            delta_x = self.TARGET[0] - aruco[1][0]
+            delta_y = self.TARGET[1] - aruco[1][1]
+            area = aruco[2]
+
+            vel_x = delta_y * self.PID
+            vel_y = delta_x * self.PID
+            vel_z = -(85000 - area) / 500000
+
+            if abs(vel_x) < self.TOL:
+                vel_x = 0.0
+            if abs(vel_y) < self.TOL:
+                vel_y = 0.0
+            if abs(vel_z) < self.TOL:
+                vel_z = 0.0
+                    
+            drone.set_vel(vel_x, vel_y, vel_z)
+            print(f"Set_vel -> x: {vel_x} y: {vel_y} z: {vel_z}")
+
+            if ((delta_x)**2 + (delta_y)**2)**0.5 < 30:
+                drone.set_vel(0, 0, 0)
+                is_centralize = True
+                print(f"Centralizou! x: {delta_x} y: {delta_y}")
+                
+            rclpy.spin_once(drone)
+        return markerID
 
 
 if __name__ == "__main__":
-    detectiontest = MarkerDetection()
-    detectiontest.qrtest(0) # recebe o id da camera a ser testada
-    #detectiontest.aruco_generator(10)
+
+    detection = MarkerDetection()
+    # detectiontest.qrtest(0) # recebe o id da camera a ser testada
+    # detectiontest.aruco_generator(10)
+
+    print("[INFO] starting video stream...")
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+
+    while True:
+        frame = vs.read()
+        list_arucos = detection.detect_arucos(frame)
+        if len(list_arucos) > 0:
+            print(list_arucos)
+
+        for tag in list_arucos:
+            cX = tag[1][0]
+            cY = tag[1][1]
+            id = tag[0]
+            cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+            cv2.putText(frame, str(id),
+                (cX + 10, cY + 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
+
+        # show the output frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+    # cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
+
     
-    #detectiontest.aruco_detection()
