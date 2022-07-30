0119c4d0366cff1562cb9440cbfbc7c96b7e4b4f
Lucas Bertan <bertan1707@gmail.com>
2022-07-30 15:18:23 -0300

    ✨ Adding subtraction feature
     Pressing the right button of the mouse, you can exclude a desired color in the mask.
     Some problems were seem in minimum values of HSV, they were too high and deleted almost every possible color.
     Because of that, those values were divided by 100 and rounded. Future calibrations of that number will be necessary.

--- scripts/calibracao_colors.py
+++ scripts/calibracao_colors.py
@@ -12,6 +12,7 @@ import matplotlib.image as mpimg
 import pyautogui
 from pynput import mouse, keyboard
 import colorsys
+import math
 
 hMin = 0
 sMin = 0
@@ -34,22 +35,26 @@ recebendoCliques = False
 key = 'a'
 
 def on_click(x, y, button, pressed):
-    if pressed and recebendoCliques:
+    
+    # add
+    global cliques
+    global cliquesMax
+    global hMax
+    global hMaxTest
+    global sMax
+    global sMaxTest
+    global vMax
+    global vMaxTest
+    global hMin
+    global hMinTest
+    global sMin
+    global sMinTest
+    global vMin
+    global vMinTest
 
-        global cliques
-        global cliquesMax
-        global hMax
-        global hMaxTest
-        global sMax
-        global sMaxTest
-        global vMax
-        global vMaxTest
-        global hMin
-        global hMinTest
-        global sMin
-        global sMinTest
-        global vMin
-        global vMinTest
+    if pressed and recebendoCliques and button == mouse.Button.left:
+
+        
 
         cliques = cliques + 1
 
@@ -84,6 +89,46 @@ def on_click(x, y, button, pressed):
             cv2.setTrackbarPos('HMin', 'Parâmetros', hMinTest)
             cv2.setTrackbarPos('SMin', 'Parâmetros', sMinTest)
             cv2.setTrackbarPos('VMin', 'Parâmetros', vMinTest)
+            cv2.setTrackbarPos('HMax', 'Parâmetros', hMaxTest)
+            cv2.setTrackbarPos('SMax', 'Parâmetros', sMaxTest)
+            cv2.setTrackbarPos('VMax', 'Parâmetros', vMaxTest)
+
+    # subtract
+    if pressed and recebendoCliques and button == mouse.Button.right:
+        cliques = cliques + 1
+
+        mousePosition = pyautogui.position()
+        RGB_color = pyautogui.pixel(mousePosition.x, mousePosition.y)
+        (r, g, b) = (RGB_color.red / 255, RGB_color.green / 255, RGB_color.blue / 255)
+        (h, s, v) = colorsys.rgb_to_hsv(r, g, b)
+        (h, s, v) = (int(h * 179), int(s * 255), int(v * 255))
+        
+        # interseção
+        if cliques == 1:
+            hMaxTest = 179
+            hMinTest = 0
+            sMaxTest = 255
+            sMinTest = 0
+            vMaxTest = 255
+            vMinTest = 0
+        else: 
+            if hMaxTest > h:
+                hMaxTest = h
+            if hMinTest < h:
+                hMinTest = h            
+            if sMaxTest > s:
+                sMaxTest = s
+            if sMinTest < s:
+                sMinTest = s
+            if vMaxTest > v:
+                vMaxTest = v
+            if vMinTest < v:
+                vMinTest = v
+        if cliques >= cliquesMax:
+            # divide por 100 para deixar mais preciso, esse numero pode ser calibrado fuuturamente
+            cv2.setTrackbarPos('HMin', 'Parâmetros', math.floor(hMinTest / 100))
+            cv2.setTrackbarPos('SMin', 'Parâmetros', math.floor(sMinTest / 100))
+            cv2.setTrackbarPos('VMin', 'Parâmetros', math.floor(vMinTest / 100))
             cv2.setTrackbarPos('HMax', 'Parâmetros', hMaxTest)
             cv2.setTrackbarPos('SMax', 'Parâmetros', sMaxTest)
             cv2.setTrackbarPos('VMax', 'Parâmetros', vMaxTest)
@@ -159,9 +204,6 @@ def setandoCliques ():
         recebendoCliques = True
         print("Cliques ativados!")
 
-def nothing(x):
-    pass
-
 def get_mask(hsv , lower_color , upper_color):
     lower = np.array(lower_color)
     upper = np.array(upper_color)
@@ -170,12 +212,13 @@ def get_mask(hsv , lower_color , upper_color):
 
     return mask
 
+def nothing(x):
+    pass
+
 capture = cv2.VideoCapture(0)
 skyratsImg = cv2.imread("./skyrats_logo.jpeg")
 skyratsImgResize = cv2.resize(skyratsImg, (300, 300))
 
-def hey (): 
-    print("hey")
 cv2.namedWindow('Camera - R: resetar máscara, E: ativar mouse e Q: sair')
 cv2.namedWindow('Parâmetros')
 
