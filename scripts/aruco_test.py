f439dfe2315fbb7e493bf134d22fbc0df3c3458c
Alessandro Menegon <95109891+minig0n@users.noreply.github.com>
2022-08-18 16:50:17 -0300

    aruco detection with real drone camera :monkey:

--- scripts/aruco_test.py
+++ scripts/aruco_test.py
@@ -1,44 +1,68 @@
-from imutils.video import VideoStream	
+#!/usr/bin/env python3
+from sensor_msgs.msg import Image
+from rclpy.node import Node
+from rclpy import qos
+import sys
+from cv_bridge import CvBridge
+import rclpy
 import cv2
-import time			
 
 from markerdetection import MarkerDetection
 
 
-def camera_test():
-	detection = MarkerDetection()
+'''
+Script for testing aruco detection on real drone camera.
 
-	print("[INFO] starting video stream...")
-	vs = VideoStream(src=8).start()
-	time.sleep(2.0)
+First, run on terminal: 
+    ros2 launch usb_cam demo_launch.py
 
-	while True:
-		frame = vs.read()
-		list_arucos = detection.aruco_detector(frame)
+This will start the image_raw ros2 topic with camera images.
+Then, this script will be able to get the messages with the subscriber and search for arucos.
 
-		if len(list_arucos) > 0:
-			print(list_arucos)
+To change the camera configurations, go to:
+    ~/skyrats_ws2/src/usb_cam2/config
 
-		for tag in list_arucos:
-			cX = tag[1][0]
-			cY = tag[1][1]
-			id = tag[0]
-			# cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
-			# cv2.putText(frame, str(id),
-				# (cX + 10, cY + 10),
-				# cv2.FONT_HERSHEY_SIMPLEX,
-				# 0.5, (0, 255, 0), 2)
+'''
 
-		# show the output frame
-		# cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
+ODROID = False
 
-		# if the `q` key was pressed, break from the loop
-		if key == ord("q"):
-			break
+class camera(Node):
 
-	# cleanup
-	# cv2.destroyAllWindows()
-	vs.stop()
+    def __init__(self):
+        super().__init__('camera')
 
-camera_test()
+        self.cam = Image()
+        self.bridge_object = CvBridge()
+        self.camera_topic = "image_raw"
+        self.cam_sub = self.create_subscription(Image, self.camera_topic, self.cam_callback, qos.qos_profile_sensor_data)
+        rclpy.spin_once(self)
+
+    def cam_callback(self, cam_data):
+        self.cam = self.bridge_object.imgmsg_to_cv2(cam_data,"bgr8")
+    
+
+if __name__ == '__main__':
+    rclpy.init(args=sys.argv)
+    cam = camera()
+    detection = MarkerDetection()
+
+    while True:
+        rclpy.spin_once(cam)
+        frame = cam.cam
+        list_of_arucos = detection.aruco_detector(frame)
+
+        print(list_of_arucos)
+
+        # Show points on frame
+        if not ODROID:
+            for tag in list_of_arucos:
+                cX = tag[1][0]
+                cY = tag[1][1]
+                id = tag[0]
+                cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+                cv2.putText(frame, str(id),(cX + 10, cY + 10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        cv2.imshow('image', frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
